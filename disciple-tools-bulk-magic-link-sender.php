613466dfa4ea2c0a582dafa791840f17c3587926
Saw Nerdar <sheep.blinker@gmail.com>
2023-05-29 15:31:24 +0630

    version update

--- disciple-tools-bulk-magic-link-sender.php
+++ disciple-tools-bulk-magic-link-sender.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Plugin Name: Ekballo Disciple Tools - Magic Links
  * Plugin URI: https://github.com/viktorsheep/ekballo-disciple-tools-bulk-magic-link-sender
  * Description: Disciple Tools - Magic Links for users, contacts, groups and teams assignment + schedule management for magic links dispatching over configured sending channels.
  * Text Domain: ekballo-disciple-tools-bulk-magic-link-sender
  * Domain Path: /languages
- * Version:  1.8.13
+ * Version:  1.9.0
  * Author URI: https://github.com/viktorsheep
  * GitHub Plugin URI: https://github.com/viktorsheep/ekballo-disciple-tools-bulk-magic-link-sender
  * Requires at least: 4.7.0
@@ -19,7 +20,7 @@
  */
 
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
@@ -30,7 +31,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since  0.1
  * @access public
  */
-function disciple_tools_bulk_magic_link_sender() {
+function disciple_tools_bulk_magic_link_sender()
+{
     $disciple_tools_magic_links_required_dt_theme_version = '1.20';
     $wp_theme                                             = wp_get_theme();
     $version                                              = $wp_theme->version;
@@ -38,28 +40,27 @@ function disciple_tools_bulk_magic_link_sender() {
     /*
      * Check if the Disciple.Tools theme is loaded and is the latest required version
      */
-    $is_theme_dt = class_exists( "Disciple_Tools" );
-    if ( $is_theme_dt && version_compare( $version, $disciple_tools_magic_links_required_dt_theme_version, "<" ) ) {
-        add_action( 'admin_notices', 'disciple_tools_magic_links_hook_admin_notice' );
-        add_action( 'wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler' );
+    $is_theme_dt = class_exists("Disciple_Tools");
+    if ($is_theme_dt && version_compare($version, $disciple_tools_magic_links_required_dt_theme_version, "<")) {
+        add_action('admin_notices', 'disciple_tools_magic_links_hook_admin_notice');
+        add_action('wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler');
 
         return false;
     }
-    if ( ! $is_theme_dt ) {
+    if (!$is_theme_dt) {
         return false;
     }
     /**
      * Load useful function from the theme
      */
-    if ( ! defined( 'DT_FUNCTIONS_READY' ) ) {
+    if (!defined('DT_FUNCTIONS_READY')) {
         require_once get_template_directory() . '/dt-core/global-functions.php';
     }
 
     return Disciple_Tools_Bulk_Magic_Link_Sender::instance();
-
 }
 
-add_action( 'after_setup_theme', 'disciple_tools_bulk_magic_link_sender', 20 );
+add_action('after_setup_theme', 'disciple_tools_bulk_magic_link_sender', 20);
 
 /**
  * Singleton class for setting up the plugin.
@@ -67,49 +68,51 @@ add_action( 'after_setup_theme', 'disciple_tools_bulk_magic_link_sender', 20 );
  * @since  0.1
  * @access public
  */
-class Disciple_Tools_Bulk_Magic_Link_Sender {
+class Disciple_Tools_Bulk_Magic_Link_Sender
+{
 
     private static $_instance = null;
 
-    public static function instance() {
-        if ( is_null( self::$_instance ) ) {
+    public static function instance()
+    {
+        if (is_null(self::$_instance)) {
             self::$_instance = new self();
         }
 
         return self::$_instance;
     }
 
-    private function __construct() {
+    private function __construct()
+    {
         $is_rest = dt_is_rest();
 
-        if ( $is_rest && strpos( dt_get_url_path(), 'disciple_tools_magic_links' ) !== false ) {
-            require_once( 'rest-api/rest-api.php' ); // adds starter rest api class
+        if ($is_rest && strpos(dt_get_url_path(), 'disciple_tools_magic_links') !== false) {
+            require_once('rest-api/rest-api.php'); // adds starter rest api class
         }
 
-        require_once( 'magic-link/magic-links-api.php' );
-        require_once( 'magic-link/magic-link-templates.php' );
-        require_once( 'magic-link/magic-link-user-app.php' );
-        require_once( 'magic-link/magic-link-user-posts-base.php' );
-        require_once( 'magic-link/magic-link-user-groups-app.php' );
-        require_once( 'magic-link/magic-links-cron.php' );
-        require_once( 'magic-link/magic-links-default-filters.php' );
+        require_once('magic-link/magic-links-api.php');
+        require_once('magic-link/magic-link-templates.php');
+        require_once('magic-link/magic-link-user-app.php');
+        require_once('magic-link/magic-link-user-posts-base.php');
+        require_once('magic-link/magic-link-user-groups-app.php');
+        require_once('magic-link/magic-links-cron.php');
+        require_once('magic-link/magic-links-default-filters.php');
 
-        if ( is_admin() ) {
-            require_once( 'admin/admin-menu-and-tabs.php' ); // adds starter admin page and section for plugin
+        if (is_admin()) {
+            require_once('admin/admin-menu-and-tabs.php'); // adds starter admin page and section for plugin
         }
 
         $this->i18n();
 
-        if ( is_admin() ) { // adds links to the plugin description area in the plugin admin list.
-            add_filter( 'plugin_row_meta', [ $this, 'plugin_description_links' ], 10, 4 );
+        if (is_admin()) { // adds links to the plugin description area in the plugin admin list.
+            add_filter('plugin_row_meta', [$this, 'plugin_description_links'], 10, 4);
         }
 
         try {
-            require_once( plugin_dir_path( __FILE__ ) . 'includes/class-migration-engine.php' );
-            Disciple_Tools_Bulk_Magic_Link_Migration_Engine::migrate( Disciple_Tools_Bulk_Magic_Link_Migration_Engine::$migration_number );
-
-        } catch ( Throwable $e ) {
-            new WP_Error( 'migration_error', 'Migration engine failed to migrate.' );
+            require_once(plugin_dir_path(__FILE__) . 'includes/class-migration-engine.php');
+            Disciple_Tools_Bulk_Magic_Link_Migration_Engine::migrate(Disciple_Tools_Bulk_Magic_Link_Migration_Engine::$migration_number);
+        } catch (Throwable $e) {
+            new WP_Error('migration_error', 'Migration engine failed to migrate.');
         }
     }
 
@@ -117,8 +120,9 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * Filters the array of row meta for each/specific plugin in the Plugins list table.
      * Appends additional links below each/specific plugin on the plugins page.
      */
-    public function plugin_description_links( $links_array, $plugin_file_name, $plugin_data, $status ) {
-        if ( strpos( $plugin_file_name, basename( __FILE__ ) ) ) {
+    public function plugin_description_links($links_array, $plugin_file_name, $plugin_data, $status)
+    {
+        if (strpos($plugin_file_name, basename(__FILE__))) {
             // You can still use `array_unshift()` to add links at the beginning.
 
             $links_array[] = '<a href="https://disciple.tools">Disciple.Tools Community</a>'; // @todo replace with your links.
@@ -135,7 +139,8 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * @since  0.1
      * @access public
      */
-    public static function activation() {
+    public static function activation()
+    {
         // add elements here that need to fire on activation
     }
 
@@ -146,9 +151,10 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * @since  0.1
      * @access public
      */
-    public static function deactivation() {
+    public static function deactivation()
+    {
         // add functions here that need to happen on deactivation
-        delete_option( 'dismissed-disciple-tools-bulk-magic-link-sender' );
+        delete_option('dismissed-disciple-tools-bulk-magic-link-sender');
     }
 
     /**
@@ -158,9 +164,10 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * @since  0.1
      * @access public
      */
-    public function i18n() {
+    public function i18n()
+    {
         $domain = 'disciple_tools_bulk_magic_link_sender';
-        load_plugin_textdomain( $domain, false, trailingslashit( dirname( plugin_basename( __FILE__ ) ) ) . 'languages' );
+        load_plugin_textdomain($domain, false, trailingslashit(dirname(plugin_basename(__FILE__))) . 'languages');
     }
 
     /**
@@ -170,7 +177,8 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * @since  0.1
      * @access public
      */
-    public function __toString() {
+    public function __toString()
+    {
         return 'disciple-tools-bulk-magic-link-sender';
     }
 
@@ -181,8 +189,9 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * @since  0.1
      * @access public
      */
-    public function __clone() {
-        _doing_it_wrong( __FUNCTION__, 'Whoah, partner!', '0.1' );
+    public function __clone()
+    {
+        _doing_it_wrong(__FUNCTION__, 'Whoah, partner!', '0.1');
     }
 
     /**
@@ -192,8 +201,9 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * @since  0.1
      * @access public
      */
-    public function __wakeup() {
-        _doing_it_wrong( __FUNCTION__, 'Whoah, partner!', '0.1' );
+    public function __wakeup()
+    {
+        _doing_it_wrong(__FUNCTION__, 'Whoah, partner!', '0.1');
     }
 
     /**
@@ -206,9 +216,10 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
      * @since  0.1
      * @access public
      */
-    public function __call( $method = '', $args = array() ) {
-        _doing_it_wrong( "disciple_tools_magic_links::" . esc_html( $method ), 'Method does not exist.', '0.1' );
-        unset( $method, $args );
+    public function __call($method = '', $args = array())
+    {
+        _doing_it_wrong("disciple_tools_magic_links::" . esc_html($method), 'Method does not exist.', '0.1');
+        unset($method, $args);
 
         return null;
     }
@@ -216,52 +227,53 @@ class Disciple_Tools_Bulk_Magic_Link_Sender {
 
 
 // Register activation hook.
-register_activation_hook( __FILE__, [ 'Disciple_Tools_Bulk_Magic_Link_Sender', 'activation' ] );
-register_deactivation_hook( __FILE__, [ 'Disciple_Tools_Bulk_Magic_Link_Sender', 'deactivation' ] );
+register_activation_hook(__FILE__, ['Disciple_Tools_Bulk_Magic_Link_Sender', 'activation']);
+register_deactivation_hook(__FILE__, ['Disciple_Tools_Bulk_Magic_Link_Sender', 'deactivation']);
 
 
-if ( ! function_exists( 'disciple_tools_magic_links_hook_admin_notice' ) ) {
-    function disciple_tools_magic_links_hook_admin_notice() {
+if (!function_exists('disciple_tools_magic_links_hook_admin_notice')) {
+    function disciple_tools_magic_links_hook_admin_notice()
+    {
         global $disciple_tools_magic_links_required_dt_theme_version;
         $wp_theme        = wp_get_theme();
         $current_version = $wp_theme->version;
         $message         = "'Disciple Tools - Magic Links' plugin requires 'Disciple Tools' theme to work. Please activate 'Disciple Tools' theme or make sure it is latest version.";
-        if ( $wp_theme->get_template() === "disciple-tools-theme" ) {
-            $message .= ' ' . sprintf( esc_html( 'Current Disciple Tools version: %1$s, required version: %2$s' ), esc_html( $current_version ), esc_html( $disciple_tools_magic_links_required_dt_theme_version ) );
+        if ($wp_theme->get_template() === "disciple-tools-theme") {
+            $message .= ' ' . sprintf(esc_html('Current Disciple Tools version: %1$s, required version: %2$s'), esc_html($current_version), esc_html($disciple_tools_magic_links_required_dt_theme_version));
         }
         // Check if it's been dismissed...
-        if ( ! get_option( 'dismissed-disciple-tools-bulk-magic-link-sender', false ) ) { ?>
-            <div class="notice notice-error notice-disciple-tools-bulk-magic-link-sender is-dismissible"
-                 data-notice="disciple-tools-bulk-magic-link-sender">
-                <p><?php echo esc_html( $message ); ?></p>
+        if (!get_option('dismissed-disciple-tools-bulk-magic-link-sender', false)) { ?>
+            <div class="notice notice-error notice-disciple-tools-bulk-magic-link-sender is-dismissible" data-notice="disciple-tools-bulk-magic-link-sender">
+                <p><?php echo esc_html($message); ?></p>
             </div>
             <script>
-                jQuery(function ($) {
-                    $(document).on('click', '.notice-disciple-tools-bulk-magic-link-sender .notice-dismiss', function () {
+                jQuery(function($) {
+                    $(document).on('click', '.notice-disciple-tools-bulk-magic-link-sender .notice-dismiss', function() {
                         $.ajax(ajaxurl, {
                             type: 'POST',
                             data: {
                                 action: 'dismissed_notice_handler',
                                 type: 'disciple-tools-bulk-magic-link-sender',
-                                security: '<?php echo esc_html( wp_create_nonce( 'wp_rest_dismiss' ) ) ?>'
+                                security: '<?php echo esc_html(wp_create_nonce('wp_rest_dismiss')) ?>'
                             }
                         })
                     });
                 });
             </script>
-        <?php }
+<?php }
     }
 }
 
 /**
  * AJAX handler to store the state of dismissible notices.
  */
-if ( ! function_exists( "dt_hook_ajax_notice_handler" ) ) {
-    function dt_hook_ajax_notice_handler() {
-        check_ajax_referer( 'wp_rest_dismiss', 'security' );
-        if ( isset( $_POST["type"] ) ) {
-            $type = sanitize_text_field( wp_unslash( $_POST["type"] ) );
-            update_option( 'dismissed-' . $type, true );
+if (!function_exists("dt_hook_ajax_notice_handler")) {
+    function dt_hook_ajax_notice_handler()
+    {
+        check_ajax_referer('wp_rest_dismiss', 'security');
+        if (isset($_POST["type"])) {
+            $type = sanitize_text_field(wp_unslash($_POST["type"]));
+            update_option('dismissed-' . $type, true);
         }
     }
 }
@@ -275,31 +287,30 @@ if ( ! function_exists( "dt_hook_ajax_notice_handler" ) ) {
  * Also, see the instructions for version updating to understand the steps involved.
  * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
  */
-add_action( 'plugins_loaded', function () {
-    if ( is_admin() && ! ( is_multisite() && class_exists( "DT_Multisite" ) ) || wp_doing_cron() ) {
+add_action('plugins_loaded', function () {
+    if (is_admin() && !(is_multisite() && class_exists("DT_Multisite")) || wp_doing_cron()) {
         // Check for plugin updates
-        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' ) ) {
-                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+        if (!class_exists('Puc_v4_Factory')) {
+            if (file_exists(get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php')) {
+                require(get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php');
             }
         }
-        if ( class_exists( 'Puc_v4_Factory' ) ) {
+        if (class_exists('Puc_v4_Factory')) {
             Puc_v4_Factory::buildUpdateChecker(
                 'https://raw.githubusercontent.com/viktorsheep/ekballo-disciple-tools-bulk-magic-link-sender/master/version-control.json',
                 __FILE__,
                 'disciple-tools-bulk-magic-link-sender'
             );
-
         }
     }
-} );
+});
 
 /**
  * Require plugins with the TGM library.
  *
  * This defines the required and suggested plugins.
  */
-add_action( 'tgmpa_register', function () {
+add_action('tgmpa_register', function () {
     /**
      * Array of plugin arrays. Required keys are name and slug.
      * If the source is NOT from the .org repo, then source is also required.
@@ -341,6 +352,5 @@ add_action( 'tgmpa_register', function () {
         // Message to output right before the plugins table.
     ];
 
-    tgmpa( $plugins, $config );
-
-} );
+    tgmpa($plugins, $config);
+});
